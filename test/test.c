9d955a38351e3074f60e08f590ec3a22b92d7735
Reverope <shatish123456789@gmail.com>
2020-12-06 00:35:06 +0530

    String Hashing Done

--- test/test.c
+++ test/test.c
@@ -2,19 +2,24 @@
 
 int main()
 {
+    char a[5] = {'F', 'i', 'r', 's', 't'};
+    char b[5] = {'S', 'e', 'c', 'o', 'n'};
+    char c[5] = {'T', 'h', 'i', 'r', 'd'};
+    char d[5] = {'F', 'o', 'u', 'r', 't'};
+
     HashTable_S *ht = create_HT_string(100);
-    insertTo_HT_string(ht, "1", "First address");
-    insertTo_HT_string(ht, "2", "Second address");
-    insertTo_HT_string(ht, "Hel", "Third address");
-    insertTo_HT_string(ht, "Cau", "Fourth address");
-    print_search(ht, "1");
-    print_search(ht, "2");
-    print_search(ht, "3");
-    print_search(ht, "Hel");
-    print_search(ht, "Cau");
+    insertTo_HT_string(ht, (char *)"1", a);
+    insertTo_HT_string(ht, (char *)"2", b);
+    insertTo_HT_string(ht, (char *)"H", c);
+    insertTo_HT_string(ht, (char *)"C", d);
+    print_search(ht, (char *)"1");
+    print_search(ht, (char *)"2");
+    print_search(ht, (char *)(char *)"3");
+    print_search(ht, (char *)"Hel");
+    print_search(ht, (char *)"Cau");
     print_table(ht);
-    delete_HT_item_string(ht, "1");
-    delete_HT_item_string(ht, "Cau");
+    delete_HT_item_string(ht, (char *)"1");
+    delete_HT_item_string(ht, (char *)"Cau");
     print_table(ht);
     free_table(ht);
     return 0;
